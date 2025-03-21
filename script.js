85686cbc7e5525cce0d51937479860f6884b0e55
gippo05 <gsarinas17@gmail.com>
2025-03-21 13:09:53 +0800

    modified

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let isAlive = true;
 let hasBlackJack = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
 
 function startGame(){
+    renderGame();
+}
+
+function renderGame(){
     if (sum <= 20){
         message = "Do you want to draw a new card?";
-        messageEl.textContent = message;
     } else if (sum === 21){
         message = "You've got Blackjack!";
         hasBlackJack = true;
-        messageEl.textContent = message;
     } else {
         message = "You're out of the game!";
-        messageEl.textContent = message;
         isAlive = false;
     }
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+}
+
+function newCard(){
+    console.log("Drawing out a new card in the deck!");
+    let card = 5;
+    sum += card;
+    renderGame();
 }
